ee4a2c9fcb5e90a25347b8e9820da8236b6a505e
Radhikagindam <148328904+Radhikagindam@users.noreply.github.com>
2025-07-21 22:34:09 +0530

    Add files via upload

---
+++ binsearch.py
@@ -0,0 +1,15 @@
+def bs(arr,target):
+    low=0
+    high=len(arr)-1
+    while(low<=high):
+        mid=(low+high)//2
+        if(arr[mid]==target):
+            return True
+        elif(arr[mid]<target):
+            low=mid+1
+        else:
+            high=mid-1
+    return False
+arr=list(map(int,input().split()))
+target=int(input())
+print(bs(arr,target))
