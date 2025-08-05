f97135e85a65aa861f4faa4fdec9ee98085fc261
Radhikagindam <148328904+Radhikagindam@users.noreply.github.com>
2025-08-05 14:39:35 +0530

    Add files via upload

---
+++ maxlen.py
@@ -0,0 +1,14 @@
+arr=list(map(int,input().split()))
+k=int(input())
+n=len(arr)
+left=0
+right=k-1
+sum=sum(arr[left:right+1])
+maxsum=sum
+while(right<n-1):
+    sum-=arr[left]
+    left+=1
+    right+=1
+    sum+=arr[right]
+    maxsum=max(maxsum,sum)
+print(maxsum)
