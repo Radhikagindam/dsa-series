69f0a8098543c89083165ef79483d1675dcb21da
Radhikagindam <148328904+Radhikagindam@users.noreply.github.com>
2025-08-08 18:20:41 +0530

    Add files via upload

---
+++ mutlilevel.py
@@ -0,0 +1,11 @@
+class GrandFather():
+    def fun1(self):
+        print("i can farm") 
+class Father(GrandFather):
+    def fun2(self):
+        print("i can do job") 
+class Son(Father):
+    def fun3(self):
+        print("i can play") 
+ob=Son() 
+ob.fun1() 
